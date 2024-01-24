25c90408774f9d06f6a2505a30448ef770ea29b0
erdin <erdin@LAPTOP-QQT4P5GF>
2024-01-24 14:14:08 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace BootCamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            double dolarDun = 7.55;
+            double dolarBugün = 7.45;
+
+            if (dolarDun > dolarBugün)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun < dolarBugün)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+            bool sistemeGirisYapmisMi = true;
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+            Console.ReadKey();
+        }
+    }
+}
